3d006bc821ef3bc7459a1f26f22847519f2316f4
unknown <ashimaacouilard@gmail.com>
2023-10-11 13:59:09 -0400

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './Asterix.bmp';
+import logo from './Asterix1.bmp';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
